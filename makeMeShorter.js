90de5072a2117d217cd2451fc2c8397d79d0858c
rezniktarasdev <75927896+rezniktarasdev@users.noreply.github.com>
2021-11-17 13:30:41 +0200

    second commit add if (space and dot)

--- makeMeShorter.js
+++ makeMeShorter.js
@@ -4,12 +4,19 @@
 // than 30 characters, the string should be returned as it is. In case the
 // parameter is not a string, the empty string should be returned.
 
-let str = 'sldkfjsdjsdfhkjsdbnksdjfgsdjfbsdkjfgsdkvbskjfgsdiufg'
+let str = '123456789012345678901234567890123'
 
 function makeMeShorter(str) {
    if (typeof str === 'string') {
       if (str.length < 30) {
          return str
+
+         //добавил доп. проверку на пробел и точку 30 символом, в условии задачи не сказано, 
+         //поэтому закомментировал
+
+         /*} if (str[29] === ' ' || str[29] === '.') {
+            return str.split('').slice(0, 29).join('') + '...';*/
+
       } else {
          return str.split('').slice(0, 30).join('') + '...';
       }
@@ -20,3 +27,4 @@ function makeMeShorter(str) {
 
 
 console.log(makeMeShorter(str));
+
